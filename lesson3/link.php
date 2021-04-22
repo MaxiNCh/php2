002c0faadb927e58a82f80a8ddab398f7a3f4d26
Maxim Zaboronkin <mvzaboronkin@gmail.com>
2021-04-22 11:17:47 +0300

    lesson4 done

--- lesson3/link.php
+++ lesson3/link.php
@@ -4,5 +4,13 @@ $DIR = './images/';
 
 $link = mysqli_connect('localhost:3306', 'root', 'MyNewPass', 'geekbrains');
 
+const ROWS_NUM = 4;
+
+try {
+    $pdo = new PDO('mysql:dbname=geekbrains;host=localhost:3306', 'root', 'MyNewPass');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $err) {
+    die("Error! " . $err->getMessage());
+}
 
 ?>
